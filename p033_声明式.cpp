1a4f05223e2d79795e43eab1d8cd49077c8b6238
nnngu <654015592@qq.com>
2020-02-02 20:12:51 +0800

    p033

---
+++ p033_声明式.cpp
@@ -0,0 +1,14 @@
+extern int x; // 对象(object)声明式
+std::size_t numDigits(int number); // 函数(function)声明式
+class Widget; // 类(class)声明式
+
+template<typename T> // 模板(template)声明式
+class GraphNode; // "typename"的使用见条款 42
+
+
+
+
+
+
+
+
