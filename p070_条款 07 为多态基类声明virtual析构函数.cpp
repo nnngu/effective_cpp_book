eb808ff24605e7d3176e8f94801edbf147404f36
nnngu <654015592@qq.com>
2020-02-18 11:45:15 +0800

    p070

--- p070_条款 07 为多态基类声明virtual析构函数.cpp
+++ p070_条款 07 为多态基类声明virtual析构函数.cpp
@@ -1,3 +1,7 @@
 /*
 条款 07 为多态基类声明virtual析构函数
+	polymorphic(带多态性质的)base classes 应该声明一个virtual析构函数。如果class带有
+任何virtual函数，它就应该拥有一个virtual析构函数。
+	Classes的设计目的如果不是作为base classes使用，或不是为了具备多态性(polymorphically),
+就不该声明virtual析构函数。
 */
