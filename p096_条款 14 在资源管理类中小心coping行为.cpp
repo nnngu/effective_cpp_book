1894ff25b0bae1088632f3664746e68a85baa1b7
nnngu <654015592@qq.com>
2020-02-26 20:31:54 +0800

    p096

--- p096_条款 14 在资源管理类中小心coping行为.cpp
+++ p096_条款 14 在资源管理类中小心coping行为.cpp
@@ -1,3 +1,8 @@
 /*
 条款 14 在资源管理类中小心coping行为
+请记住：
+	复制 RAII 对象必须一并复制它所管理的资源，所以资源的 coping 行为决定
+RAII 对象的 coping 行为。
+	普遍而常见的 RAII class copying 行为是：抑制 coping、施行引用计数法
+(reference counting)。不过其他行为也都可能被实现。
 */
