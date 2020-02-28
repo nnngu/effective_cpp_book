de13f0b0c52ae46bc97124dcc36332adeb0d1785
nnngu <654015592@qq.com>
2020-02-28 13:43:55 +0800

    p105 p108

--- p105_条款 17 以独立语句将newed对象置入智能指针.cpp
+++ p105_条款 17 以独立语句将newed对象置入智能指针.cpp
@@ -1,4 +1,13 @@
 /*
 条款 17 以独立语句将newed对象置入智能指针
-
+请记住：
+	以独立语句将newed对象存储于(置入)智能指针内。如果不这样做，一旦异常被抛出，
+有可能导致难以察觉的资源泄露。
 */
+
+// 不建议的写法：
+processWidget(std::tr1::shared_ptr<Widget>(new Widget), priority()); // 不建议
+
+// 建议如下写法：
+std::tr1::shared_ptr<Widget> pw(new Widget);
+processWidget(pw, priority());
