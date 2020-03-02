211ab323480a41d89305bda725ad83c75e7d8d44
nnngu <654015592@qq.com>
2020-03-02 14:28:51 +0800

    p116 p120

--- p116_条款 20 宁以pass-by-reference-to-const替换pass-by-value.cpp
+++ p116_条款 20 宁以pass-by-reference-to-const替换pass-by-value.cpp
@@ -1,4 +1,8 @@
 /*
 条款 20 宁以pass-by-reference-to-const替换pass-by-value
-
+请记住：
+	尽量以pass-by-reference-to-const替换pass-by-value。前者通常比较高效，
+并可避免切割问题(slicing problem)。
+	以上规则并不适用于内置类型，以及STL的迭代器和函数对象。对它们而言，
+pass-by-value往往比较适当。
 */
