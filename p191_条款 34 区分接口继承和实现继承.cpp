fcb76fe0e94a9635f6c3dd3fb053758578fc7ccc
nnngu <654015592@qq.com>
2020-03-10 15:12:17 +0800

    p191 p199

--- p191_条款 34 区分接口继承和实现继承.cpp
+++ p191_条款 34 区分接口继承和实现继承.cpp
@@ -1,4 +1,8 @@
 /*
 条款 34 区分接口继承和实现继承
-
+请记住：
+	接口继承和实现继承不同。在public继承之下，derived classes总是继承base class的接口。
+	pure virtual函数只具体指定接口继承。
+	简朴的(非纯)impure virtual函数具体指定接口继承及缺省实现继承。
+	non-virtual函数具体指定接口继承以及强制性实现继承。
 */
