8d7fc6d7dee5735f2897715661cfd4fbf26d79c1
nnngu <654015592@qq.com>
2020-03-11 15:37:54 +0800

    p214 p217

--- p214_条款 38 通过复合塑模出has-a或根据某物实现出.cpp
+++ p214_条款 38 通过复合塑模出has-a或根据某物实现出.cpp
@@ -1,4 +1,7 @@
 /*
 条款 38 通过复合塑模出has-a或根据某物实现出
-
+请记住：
+	复合(composition)的意义和public继承完全不同。
+	在应用域(application domain)，复合意味has-a(有一个)。在实现域(implementation domain)，
+复合意味is-implemented-in-terms-of(根据某物实现出)。
 */
