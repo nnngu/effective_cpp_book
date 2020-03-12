6e0be7b136829e6893c72a5aa34b2b93d191d68f
nnngu <654015592@qq.com>
2020-03-12 18:02:43 +0800

    p237

--- p237_条款 43 学习处理模板化基类内的名称.cpp
+++ p237_条款 43 学习处理模板化基类内的名称.cpp
@@ -2,3 +2,43 @@
 条款 43 学习处理模板化基类内的名称
 
 */
+class CompanyA {
+public:
+	void sendCleartext(const std::string& msg);
+	void sendEncryted(const std::string& msg);
+};
+class CompanyB {
+public:
+	void sendCleartext(const std::string& msg);
+	void sendEncryted(const std::string& msg);
+};
+class MsgInfo {};
+
+template<typename Company>
+class MsgSender {
+public:
+	... // 构造函数、析构函数等等。
+	void sendClear(const MsgInfo& info)
+	{
+		std::string msg;
+		Company c;
+		c.sendCleartext(msg);
+	}
+	void sendSecret(const MsgInfo& info)
+	{}
+};
+
+// -------------------------
+
+class CompanyZ {
+public:
+	...
+	void sendEncryted(const std::string& msg);
+	...
+};
+
+
+
+
+
+
