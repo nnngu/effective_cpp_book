ded054ccb50062fbfad8701414c7f286a0d51aa1
nnngu <654015592@qq.com>
2020-03-12 17:14:10 +0800

    p233 p237

--- p233_条款 42 了解typename的双重意义.cpp
+++ p233_条款 42 了解typename的双重意义.cpp
@@ -1,4 +1,7 @@
 /*
 条款 42 了解typename的双重意义
-
+请记住：
+	声明template参数时，前缀关键字class和typename可互换。
+	请使用关键字typename标识嵌套从属类型名称；但不得在base class lists(基类列)
+或member initialization list(成员初值列)内以它作为base class修饰符。
 */
