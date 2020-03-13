f4e996d99f9993df0bb1c925832c3dfe6aeba7ec
nnngu <654015592@qq.com>
2020-03-13 17:57:56 +0800

    p248 p252

--- p248_条款 45 运用成员函数模板接受所有兼容类型.cpp
+++ p248_条款 45 运用成员函数模板接受所有兼容类型.cpp
@@ -1,4 +1,7 @@
 /*
 条款 45 运用成员函数模板接受所有兼容类型
-
+请记住：
+	请使用member function templates(成员函数模板)生成"可接受所有兼容类型"的函数。
+	如果你声明member templates 用于"泛化copy构造"或"泛化assignment操作"，你还是需要声明
+正常的copy构造函数和copy assignment操作符。
 */
