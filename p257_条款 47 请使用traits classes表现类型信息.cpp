2de0c9629d4f4cc870f1cd378d0f456bb65714fa
nnngu <654015592@qq.com>
2020-03-16 18:06:03 +0800

    p257

--- p257_条款 47 请使用traits classes表现类型信息.cpp
+++ p257_条款 47 请使用traits classes表现类型信息.cpp
@@ -2,3 +2,13 @@
 条款 47 请使用traits classes表现类型信息
 
 */
+
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag: public input_iterator_tag {};
+struct bidirectional_iterator_tag: public forward_iterator_tag {};
+struct random_access_iterator_tag: public bidirectional_iterator_tag {};
+
+
+
+
