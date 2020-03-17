21f28c46c006a9b6de4fe0a30508a2da9d4a8c77
nnngu <654015592@qq.com>
2020-03-17 11:53:59 +0800

    p263 p270

--- p263_条款 48 认识template元编程.cpp
+++ p263_条款 48 认识template元编程.cpp
@@ -1,4 +1,8 @@
 /*
 条款 48 认识template元编程
-
+请记住：
+	Template metaprogramming(TMP，模板元编程)可将工作由运行期移往编译期，
+因而得以实现早期错误侦测和更高的执行效率。
+	TMP可被用来生成"基于政策选择组合"(based on combinations of policy choices)的
+客户定制代码，也可用来避免生成对某些特殊类型并不适合的代码。
 */
