c16e427713aa0167e8c41e735bb395dfd0edfbd7
nnngu <654015592@qq.com>
2020-03-19 11:47:38 +0800

    p293 p299

--- p293_条款 54 让自己熟悉包括TR1在内的标准程序库.cpp
+++ p293_条款 54 让自己熟悉包括TR1在内的标准程序库.cpp
@@ -1,4 +1,8 @@
 /*
 条款 54 让自己熟悉包括TR1在内的标准程序库
-
+请记住：
+	C++标准库的主要机能由STL、iostreams、locales组成。并包含C99标准库。
+	TR1添加了智能指针(例如tr1::shared_ptr)、一般化函数指针(tr1::function)、hash-based容器、
+正则表达式(regular expressions)以及另外10个组件的支持。
+	TR1自身只是一份规范。为获得TR1提供的好处，你需要一份实物。一个好的实物来源是Boost。
 */
